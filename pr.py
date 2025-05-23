e5e34832f174760dbe78f42eb317ab7cebc814cf
TatianaG01 <gorenko_tatiana@knu.ua>
2025-05-23 23:08:09 +0300

    set_shape, choose_color, start_draw

--- pr.py
+++ pr.py
@@ -1,7 +1,17 @@
+from tkinter import colorchooser
 
 
+def set_shape(self, shape):
+    self.shape = shape
 
+def choose_color(self):
+    color = colorchooser.askcolor()[1]
+    if color:
+        self.color = color
 
+def start_draw(self, event):
+    self.start_x = event.x
+    self.start_y = event.y
 
 
 
